3b7cba5ca85b5bc8d76d250270144254203d655e
Fabrice Creuzot <code@luigifab.fr>
2020-01-02 13:59:13 +0100

    Version 2.5.0

---
+++ app/javascripts/bbcode.js
@@ -0,0 +1,309 @@
+/**
+ * Created J/19/08/2010
+ * Updated J/13/10/2011
+ * Version 9
+ *
+ * Copyright 2008-2011 | Fabrice Creuzot (luigifab) <code~luigifab~info>
+ * http://www.luigifab.info/apijs
+ *
+ * This program is free software, you can redistribute it or modify
+ * it under the terms of the GNU General Public License (GPL) as published
+ * by the free software foundation, either version 2 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty, without even the implied warranty of
+ * merchantability or fitness for a particular purpose. See the
+ * GNU General Public License (GPL) for more details.
+ */
+
+function BBcode() {
+
+	// définition des attributs
+	this.bbcode = null;
+	this.object = null;
+	this.fragment = null;
+	this.emotes = false;
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// GESTION DES DONNÉES (3)
+
+	// #### Initialisation ########################################################## public ### //
+	// = révision : 11
+	// » Prépare l'objet de transition
+	// » Ajoute un conteneur p lorsque nécessaire
+	this.init = function (data, emotes) {
+
+		this.object = { tag: 'div', content: [] };
+		this.object['class'] = 'bbcode';
+
+		this.bbcode = (data[0] !== '[') ? '[p]' + data + '[/p]' : data;
+
+		if ((apijs.config.bbcode !== null) && (typeof apijs.config.bbcode === 'object'))
+			this.emotes = (typeof emotes === 'boolean') ? emotes : true;
+	};
+
+
+	// #### Interprétation ########################################################## public ### //
+	// = révision : 2
+	// » Analyse le bbcode
+	// » Génère le fragment DOM
+	this.exec = function () {
+
+		this.readData(this.bbcode, 0);
+
+		this.fragment = document.createDocumentFragment();
+		this.fragment.appendChild(this.createDomFragment(this.object));
+	};
+
+
+	// #### Résultat ################################################################ public ### //
+	// = révision : 5
+	// » Renvoie le fragment DOM correspondant au bbcode
+	// » Même si celui-ci est null
+	this.get = function () {
+
+		return this.fragment;
+	};
+
+
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// GÉNÉRATION DE L'OBJET (3)
+
+	// #### Création de l'objet de transition ###################################### private ### //
+	// = révision : 15
+	// » Parcours le bbcode récursivement
+	// » Découpe les données avec des expressions régulières
+	// » Sauvegarde chaque élément et chaque bout de texte dans un tableau d'objets
+	// » À bien noter qu'en JavaScript, les objets sont passés par référence, ils ne sont jamais copiés
+	this.readData = function (data, level) {
+
+		var element = null, attributes = null, content = null, text = null, other = null, cut = 0;
+
+		// *** Texte et éléments simples ou doubles ************* //
+		// la chaine contient du texte suivit d'un ou plusieurs éléments
+		// extrait le premier bout de texte, le premier élément et son contenu ainsi que ce qu'il y a après
+		// auto-rappel pour analyser chaque morceau
+		if ((data[0] !== '[') && ((cut = data.search(/\[([a-z1-6]+)(?: [a-z:]+=["'][^\]]*["'])*\]/)) > -1)) {
+
+			// texte (auto-rappel)
+			text = data.slice(0, cut);
+			this.readData(text, level);
+
+			// élément double (auto-rappel)
+			other = data.slice(cut);
+
+			if ((cut = other.indexOf('[/' + RegExp.$1 + ']')) > -1) {
+				element = other.slice(0, cut + RegExp.$1.length + 3);
+				other = other.slice(cut + RegExp.$1.length + 3);
+				this.readData(element, level);
+			}
+
+			// élément simple (auto-rappel)
+			if (/^(\[(?:area|br|col|hr|iframe|img|input|param)(?: [a-z:]+=["'][^\]]*["'])*\])/.test(other)) {
+				element = other.slice(0, RegExp.$1.length);
+				other = other.slice(RegExp.$1.length);
+				this.readData(element, level);
+			}
+
+			// ce qu'il reste (auto-rappel)
+			if (other.length > 0)
+				this.readData(other, level);
+		}
+
+		// *** Élément simple *********************************** //
+		// la chaine contient un élément simple en première position
+		// extrait l'élément et ses attributs ainsi que ce qu'il y a après
+		// demande la sauvegarde de l'élément et de ses attributs
+		// auto-rappel pour analyser ce qu'il y a après
+		else if (/^\[(area|br|col|hr|iframe|img|input|param)((?: [a-z:]+=["'][^\]]*["'])*)\]/.test(data)) {
+
+			element = RegExp.$1;
+			attributes = RegExp.$2;
+
+			other = data.slice(2 + element.length + attributes.length);
+			this.addElement(element, attributes, level);
+
+			if (other.length > 0)
+				this.readData(other, level);
+		}
+
+		// *** Élément double *********************************** //
+		// la chaine contient un élément double en première position
+		// extrait l'élément et ses attributs, son contenu ainsi que ce qu'il y a après
+		// demande la sauvegarde de l'élément, de son contenu et de ses attributs
+		// auto-rappel pour analyser son contenu et ce qu'il y a après
+		else if (/^\[([a-z1-6]+)((?: [a-z:]+=["'][^\]]*["'])*)\]/.test(data)) {
+
+			element = RegExp.$1;
+			attributes = RegExp.$2;
+
+			cut = data.indexOf('[/' + element + ']');
+			content = data.slice(2 + element.length + attributes.length, cut);
+			other = data.slice(3 + element.length + cut);
+
+			this.addElement(element, attributes, level);
+			this.readData(content, level + 1);
+
+			if (other.length > 0)
+				this.readData(other, level);
+		}
+
+		// *** Texte ******************************************** //
+		// demande la sauvegarde du bout de texte
+		else {
+			this.addElement(data, null, level);
+		}
+	};
+
+
+	// #### Ajoute un nœud élément ou un nœud texte ################################ private ### //
+	// = révision : 10
+	// » Enregistre le nœud élément et ses attributs ou le nœud texte dans le tableau d'objets
+	// » Remplace les émoticônes lorsque nécessaire en fonction de la configuration
+	// » À bien noter qu'en JavaScript, les objets sont passés par référence, ils ne sont jamais copiés
+	this.addElement = function (data, attributes, level) {
+
+		var directlink = null, attr = null, name = null, value = null, i = 0, hasEmotes = false, dataArray = null;
+		directlink = this.getContentNode(this.object, 0, level);
+
+		// *** Nœud élément ************************************* //
+		if (attributes !== null) {
+
+			if (directlink.hasOwnProperty('content'))
+				directlink.content.push({ tag: data });
+			else
+				directlink.content = [{ tag: data }];
+
+			if (attributes.length > 5) {
+				attributes = attributes.slice(1, -1).split(/["'] /);
+
+				for (attr in attributes) if (attributes.hasOwnProperty(attr)) {
+
+					name = attributes[attr].slice(0, attributes[attr].indexOf('='));
+					value = attributes[attr].slice(attributes[attr].indexOf('=') + 2);
+
+					if (directlink.hasOwnProperty('content'))
+						directlink.content[directlink.content.length - 1][name] = value;
+					else
+						directlink[name] = value;
+				}
+			}
+		}
+
+		// *** Nœud texte *************************************** //
+		else {
+			// recherche des éventuels émoticônes
+			if (this.emotes) {
+
+				dataArray = data.split(' ');
+
+				for (value in dataArray) {
+					if (typeof dataArray[value] !== 'string')
+						continue;
+					if (apijs.config.bbcode.hasOwnProperty(dataArray[value])) {
+						hasEmotes = true;
+						currentEmote = apijs.config.bbcode[dataArray[value]];
+						dataArray[value] = '[img src="' + currentEmote.src + '" width="' + currentEmote.width + '" height="' + currentEmote.height + '" alt="' + dataArray[value] + '" class="emote"]';
+					}
+					else if (apijs.config.bbcode.hasOwnProperty(dataArray[value].slice(0, -1))) {
+						hasEmotes = true;
+						currentEmote = apijs.config.bbcode[dataArray[value].slice(0, -1)];
+						dataArray[value] = '[img src="' + currentEmote.src + '" width="' + currentEmote.width + '" height="' + currentEmote.height + '" alt="' + dataArray[value] + '" class="emote"]' + dataArray[value].slice(-1);
+					}
+				}
+
+				data = dataArray.join(' ');
+
+				if (hasEmotes)
+					this.readData(data, level);
+				else if (directlink.hasOwnProperty('content'))
+					directlink.content.push({ text: data });
+				else
+					directlink.content = [{ text: data }];
+			}
+			// texte seul
+			else {
+				if (directlink.hasOwnProperty('content'))
+					directlink.content.push({ text: data });
+				else
+					directlink.content = [{ text: data }];
+			}
+		}
+	};
+
+
+	// #### Recherche le dernier nœud content ###################################### private ### //
+	// = révision : 4
+	// » Recherche le dernier nœud content de l'objet
+	// » Ne va pas plus loin que le niveau maximum demandé
+	this.getContentNode = function (dom, level, maxlevel) {
+
+		if ((dom.content.length < 1) || (maxlevel < 1))
+			return dom;
+
+		for (var i = dom.content.length - 1; i >= 0; i--) {
+
+			if (dom.content[i].hasOwnProperty('content') && (++level < maxlevel))
+				return this.getContentNode(dom.content[i], level, maxlevel);
+			else
+				return dom.content[i];
+		}
+	};
+
+
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// GÉNÉRATION DU FRAGMENT DOM (1)
+
+	// #### Création du fragment DOM ############################################### private ### //
+	// = révision : 14
+	// » Crée récursivement les différents nœuds à partir du tableau d'objets
+	// » Prend en charge les nœuds éléments et leurs attributs ainsi que les nœuds textes
+	// » À bien noter qu'en JavaScript, les objets sont passés par référence, ils ne sont jamais copiés
+	// » http://jsperf.com/create-nested-dom-structure
+	this.createDomFragment = function (data) {
+
+		var tag = null, attr = null, elem = 0;
+
+		// prépare un nœud élément ou renvoie un nœud texte
+		if (data.hasOwnProperty('tag'))
+			tag = document.createElement(data.tag);
+		else
+			return document.createTextNode(data.text);
+
+		// extraction des données de l'élément
+		// ajoute ses attributs et son nœud texte si nécessaire
+		// prend en charge les liens à ouvrir dans un nouvel onglet
+		for (attr in data) if (data.hasOwnProperty(attr)) {
+
+			if (attr === 'text')
+				tag.appendChild(document.createTextNode(data[attr]));
+
+			if ((attr !== 'tag') && (attr !== 'text') && (attr !== 'content'))
+				tag.setAttribute(attr, data[attr]);
+
+			if ((data.tag === 'a') && (attr === 'class') && (data[attr].indexOf('popup') > -1) && (typeof openTab === 'function')) {
+
+				if (apijs.config.navigator)
+					tag.addEventListener('click', openTab, false);
+				else
+					tag.setAttribute('onclick', 'window.open(this.href); return false;');
+			}
+		}
+
+		// l'élément contient un ou plusieurs sous éléments
+		// auto-rappel pour analyser chaque élément
+		if (data.hasOwnProperty('content')) {
+
+			for (elem = 0; elem < data.content.length; elem++)
+				tag.appendChild(this.createDomFragment(data.content[elem]));
+		}
+
+		return tag;
+	};
+}
---
+++ app/javascripts/slideshow.js
@@ -0,0 +1,368 @@
+/**
+ * Created J/13/05/2010
+ * Updated S/03/09/2011
+ * Version 17
+ *
+ * Copyright 2008-2011 | Fabrice Creuzot (luigifab) <code~luigifab~info>
+ * http://www.luigifab.info/apijs
+ *
+ * This program is free software, you can redistribute it or modify
+ * it under the terms of the GNU General Public License (GPL) as published
+ * by the free software foundation, either version 2 of the license, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty, without even the implied warranty of
+ * merchantability or fitness for a particular purpose. See the
+ * GNU General Public License (GPL) for more details.
+ */
+
+function Slideshow() {
+
+	// définition des attributs
+	this.presentation = null;
+	this.totals = null;
+	this.media = null;
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// GESTION DU DIAPORAMA (5)
+
+	// #### Initialisation ########################################################## public ### //
+	// = révision : 13
+	// » Recherche les albums et les photos et vidéos de chaque album
+	// » Met en place les gestionnaires d'événements associés lorsque nécessaire
+	// » Seul les albums en mode présentation réagiront au survol si la config le permet
+	// » Enregistre le numéro du dernier élément de chaque album
+	// » Enregistre également si l'album est en mode présentation ou pas
+	this.init = function () {
+
+		this.media = { album: null, number: null, first: null, prev: null, next: null, last: null };
+		this.presentation = [];
+		this.totals = [];
+
+		for (var i = 0, j = 0, id = null; document.getElementById(apijs.config.slideshow.ids + '.' + i) !== null; i++) {
+
+			// *** Détection du mode présentation ************** //
+			id = apijs.config.slideshow.ids + '.' + i + '.999';
+			this.presentation[i] = (document.getElementById(id)) ? 0 : false;
+
+			if (document.getElementById(id)) {
+
+				// eventListener:click
+				if (apijs.config.navigator)
+					document.getElementById(id).addEventListener('click', apijs.slideshow.showMedia, false);
+				else
+					document.getElementById(id).setAttribute('onclick', "apijs.slideshow.showMedia(this.getAttribute('id')); return false;");
+			}
+
+			// *** Recherche des éléments de l'album *********** //
+			for (j = 0; document.getElementById(apijs.config.slideshow.ids + '.' + i + '.' + j) !== null; j++) {
+
+				id = apijs.config.slideshow.ids + '.' + i + '.' + j;
+
+				if (apijs.config.navigator) {
+					// eventListener:click et eventListener:mouseover
+					if (apijs.config.slideshow.hoverload && (this.presentation[i] !== false)) {
+						document.getElementById(id).addEventListener('click', apijs.slideshow.showMedia, false);
+						document.getElementById(id).addEventListener('mouseover', apijs.slideshow.showMedia, false);
+					}
+					// eventListener:click
+					else {
+						document.getElementById(id).addEventListener('click', apijs.slideshow.showMedia, false);
+					}
+				}
+				else {
+					// onclick et onmouseover
+					if (apijs.config.slideshow.hoverload && (this.presentation[i] !== false)) {
+						document.getElementById(id).setAttribute('onclick', "apijs.slideshow.showMedia(this.getAttribute('id')); return false;");
+						document.getElementById(id).setAttribute('onmouseover', "apijs.slideshow.showMedia(this.getAttribute('id')); return false;");
+					}
+					// onclick
+					else {
+						document.getElementById(id).setAttribute('onclick', "apijs.slideshow.showMedia(this.getAttribute('id')); return false;");
+					}
+				}
+
+				this.totals[i] = j;
+			}
+		}
+	};
+
+
+	// #### Prépare l'affichage du dialogue ############## i18n ## event ## debug ## private ### //
+	// = révision : 36
+	// » Recherche les informations de la photo ou de la vidéo à afficher
+	// » En mode diaporama, déduit s'il faut mettre à jour l'image principale de l'album, s'il faut afficher un dialogue photo ou vidéo
+	//  ou s'il faut mettre à jour les dialogues photo ou vidéo
+	// » S'assure également de ne pas faire deux fois la même chose
+	this.showMedia = function (ev) {
+
+		// *** Recherche des informations *********************** //
+		var theMedia = { id: null, url: null, conf: null, num: 0, name: null, date: null, legend: null, width: null, height: null }, tmp = null;
+
+		if (typeof ev !== 'string') {
+			ev.preventDefault();
+			theMedia.id  = this.getAttribute('id');
+			theMedia.url = this.getAttribute('href');
+			theMedia.conf = this.getElementsByTagName('input')[0].getAttribute('value').split('|');
+
+			tmp = theMedia.id.split('.');
+			theMedia.album = parseInt(tmp[1], 10);
+			theMedia.number = parseInt(tmp[2], 10);
+		}
+		else {
+			theMedia.id  = ev;
+			theMedia.url = document.getElementById(theMedia.id).getAttribute('href');
+			theMedia.conf = document.getElementById(theMedia.id).getElementsByTagName('input')[0].getAttribute('value').split('|');
+
+			tmp = theMedia.id.split('.');
+			theMedia.album = parseInt(tmp[1], 10);
+			theMedia.number = parseInt(tmp[2], 10);
+		}
+
+		// *** Affichage du dialogue (mode présentation) ******** //
+		if ((apijs.slideshow.presentation[theMedia.album] !== false) && ((theMedia.conf.length > 1) || (theMedia.conf.length < 7))) {
+
+			// annulation si l'élément possède la classe actif
+			// survol ou clic
+			if ((typeof ev !== 'string') && this.getElementsByTagName('img')[0].hasAttribute('class') &&
+			    (this.getElementsByTagName('img')[0].getAttribute('class').indexOf('actif') > -1))
+				return;
+
+			// mise à jour de l'image principale
+			// survol ou clic
+			if ((apijs.dialogue.dialogType === null) && ((theMedia.conf.length === 6) || (theMedia.conf.length === 4))) {
+				apijs.slideshow.updatePresentation(theMedia);
+			}
+
+			// affichage du dialogue
+			// clic sur l'image principale
+			else if ((apijs.dialogue.dialogType === null) && ((theMedia.conf.length === 5) || (theMedia.conf.length === 3))) {
+				theMedia.number = apijs.slideshow.presentation[theMedia.album];
+				apijs.slideshow.showDialogue(theMedia);
+			}
+
+			// changement de photo ou de vidéo depuis le dialogue
+			// touches droite gauche début fin et boutons précédent suivant
+			else {
+				apijs.slideshow.updatePresentation(theMedia);
+				apijs.slideshow.showDialogue(theMedia);
+			}
+		}
+
+		// *** Affichage du dialogue **************************** //
+		else if ((theMedia.conf.length === 5) || (theMedia.conf.length === 3))
+			apijs.slideshow.showDialogue(theMedia);
+
+		// *** Message de debug ********************************* //
+		else if (apijs.config.debug)
+			apijs.dialogue.dialogInformation(apijs.i18n.translate('debugInvalidUse'), '[pre]TheSlideshow » showMedia[br]' + apijs.i18n.translate('debugNotRecognizedConfig') + '[/pre]');
+	};
+
+
+	// #### Gestion du mode présentation ########################## i18n ## debug ## private ### //
+	// = révision : 15
+	// » Extrait et vérifie les données nécessaires à la modification des attributs de l'image principale de l'album
+	// » Se base sur le lien de l'image qui vient d'être cliqué ou survolé
+	// » Ajoute l'attribut classe actif sur l'image du lien en question
+	// » Enregistre le numéro de l'image en question
+	this.updatePresentation = function (theMedia) {
+
+		var id = null, tag = null, i = 0;
+
+		// *** Image principale (photo) ************************* //
+		if ((theMedia.conf.length === 6) && (theMedia.conf[0].length > 0) && (theMedia.conf[1].length > 0) && (theMedia.conf[2].length > 0) &&
+		    (theMedia.conf[3].length > 0) && (theMedia.conf[4].length > 0)) {
+
+			// mise à jour des attributs
+			id = apijs.config.slideshow.ids + '.' + theMedia.album;
+			document.getElementById(id + '.999').setAttribute('href', document.getElementById(theMedia.id).getAttribute('href'));
+			document.getElementById(id + '.999').getElementsByTagName('img')[0].setAttribute('src', theMedia.conf.shift());
+			document.getElementById(id + '.999').getElementsByTagName('input')[0].setAttribute('value', theMedia.conf.join('|'));
+
+			// classe actif
+			for (tag = document.getElementById(id).getElementsByTagName('img'), i = 0; i < tag.length; i++) {
+
+				if (tag[i].hasAttribute('class') && (tag[i].getAttribute('class').indexOf('actif') > -1))
+					tag[i].removeAttribute('class');
+			}
+
+			document.getElementById(theMedia.id).getElementsByTagName('img')[0].setAttribute('class', 'actif');
+			this.presentation[theMedia.album] = theMedia.number;
+		}
+
+		// *** Message de debug ********************************* //
+		else if (apijs.config.debug && (theMedia.conf.length === 6)) {
+
+			apijs.dialogue.dialogInformation(apijs.i18n.translate('debugInvalidConfig'), '[pre]TheSlideshow » changePhoto[br]➩ (string) url : ' + theMedia.conf[0] + '[br]➩ (number) width : ' + theMedia.conf[1] + '[br]➩ (number) height : ' + theMedia.conf[2] + '[br]➩ (string) date : ' + theMedia.conf[3] + '[br]➩ (string) legend : ' + theMedia.conf[4] + '[/pre]');
+		}
+
+		// *** Image principale (vidéo) ************************* //
+		else if ((theMedia.conf.length === 4) && (theMedia.conf[0].length > 0) && (theMedia.conf[1].length > 0) &&
+		         (theMedia.conf[2].length > 0)) {
+
+			// mise à jour des attributs
+			id = apijs.config.slideshow.ids + '.' + theMedia.album;
+			document.getElementById(id + '.999').setAttribute('href', document.getElementById(theMedia.id).getAttribute('href'));
+			document.getElementById(id + '.999').getElementsByTagName('img')[0].setAttribute('src', theMedia.conf.shift());
+			document.getElementById(id + '.999').getElementsByTagName('input')[0].setAttribute('value', theMedia.conf.join('|'));
+
+			// classe actif
+			for (tag = document.getElementById(id).getElementsByTagName('img'), i = 0; i < tag.length; i++) {
+
+				if (tag[i].hasAttribute('class') && (tag[i].getAttribute('class').indexOf('actif') > -1))
+					tag[i].removeAttribute('class');
+			}
+
+			document.getElementById(theMedia.id).getElementsByTagName('img')[0].setAttribute('class', 'actif');
+			this.presentation[theMedia.album] = theMedia.number;
+		}
+
+		// *** Message de debug ********************************* //
+		else if (apijs.config.debug && (theMedia.conf.length === 4)) {
+
+			apijs.dialogue.dialogInformation(apijs.i18n.translate('debugInvalidConfig'), '[pre]TheSlideshow » changePhoto[br]➩ (string) url : ' + theMedia.conf[0] + '[br]➩ (string) date : ' + theMedia.conf[1] + '[br]➩ (string) legend : ' + theMedia.conf[2] + '[/pre]');
+		}
+	};
+
+
+	// #### Affichage du dialogue ################################# i18n ## debug ## private ### //
+	// = révision : 19
+	// » Extrait et vérifie les données nécessaires à l'affichage de la photo ou vidéo
+	// » Affiche une photo ou une vidéo grâce aux dialogues photo ou vidéo de [TheDialogue]
+	// » Dans tout les cas remplace l'ancien dialogue par un nouveau
+	this.showDialogue = function (theMedia) {
+
+		// *** Dialogue photo *********************************** //
+		if ((theMedia.conf.length === 5) && (theMedia.conf[0].length > 0) && (theMedia.conf[1].length > 0) && (theMedia.conf[2].length > 0) &&
+		    (theMedia.conf[3].length > 0)) {
+
+			// extraction des données
+			theMedia.name   = theMedia.conf[2];
+			theMedia.date   = theMedia.conf[3];
+			theMedia.legend = theMedia.conf[4];
+			theMedia.width  = parseInt(theMedia.conf[0], 10);
+			theMedia.height = parseInt(theMedia.conf[1], 10);
+
+			// suppression de l'ancien dialogue
+			if (apijs.dialogue.dialogType !== null)
+				apijs.dialogue.actionClose(false);
+
+			// mise en place du dialogue photo
+			apijs.dialogue.dialogPhoto(theMedia.width, theMedia.height, theMedia.url, theMedia.name, theMedia.date, theMedia.legend, true);
+			this.showNavigation(theMedia.album, theMedia.number);
+		}
+
+		// *** Message de debug ********************************* //
+		else if (apijs.config.debug && (theMedia.conf.length === 5)) {
+
+			apijs.dialogue.dialogInformation(apijs.i18n.translate('debugInvalidConfig'), '[pre]TheSlideshow » showMedia[br]➩ (number) width : ' + theMedia.conf[0] + '[br]➩ (number) height : ' + theMedia.conf[1] + '[br]➩ (string) date : ' + theMedia.conf[2] + '[br]➩ (string) legend : ' + theMedia.conf[3] + '[/pre]');
+		}
+
+		// *** Dialogue vidéo *********************************** //
+		else if ((theMedia.conf.length === 3) && (theMedia.conf[0].length > 0) && (theMedia.conf[1].length > 0)) {
+
+			// extraction des données
+			theMedia.name   = theMedia.conf[0];
+			theMedia.date   = theMedia.conf[1];
+			theMedia.legend = theMedia.conf[2];
+
+			// suppression de l'ancien dialogue
+			if (apijs.dialogue.dialogType !== null)
+				apijs.dialogue.actionClose(false);
+
+			// mise en place du dialogue vidéo
+			apijs.dialogue.dialogVideo(theMedia.url, theMedia.name, theMedia.date, theMedia.legend, true);
+			this.showNavigation(theMedia.album, theMedia.number);
+		}
+
+		// *** Message de debug ********************************* //
+		else if (apijs.config.debug && (theMedia.conf.length === 3)) {
+
+			apijs.dialogue.dialogInformation(apijs.i18n.translate('debugInvalidConfig'), '[pre]TheSlideshow » showMedia[br]➩ (string) date : ' + theMedia.conf[0] + '[br]➩ (string) legend : ' + theMedia.conf[1] + '[/pre]');
+		}
+	};
+
+
+	// #### Affichage des boutons de navigation #################################### private ### //
+	// = révision : 24
+	// » Affiche les boutons précédent et suivant si nécessaire
+	// » Vérifie au préalable s'il existe une photo ou vidéo précédente et s'il existe une photo ou vidéo suivante
+	// » S'assure qu'un dialogue photo ou vidéo est présent avant de faire n'importe quoi
+	this.showNavigation = function (album, number) {
+
+		if ((apijs.dialogue.dialogType.indexOf('photo') > -1) || (apijs.dialogue.dialogType.indexOf('video') > -1)) {
+
+			// préparation des variables
+			this.media.album = album;
+			this.media.number = number;
+
+			this.media.first = apijs.config.slideshow.ids + '.' + this.media.album + '.0';
+			this.media.prev  = apijs.config.slideshow.ids + '.' + this.media.album + '.' + (this.media.number - 1);
+			this.media.next  = apijs.config.slideshow.ids + '.' + this.media.album + '.' + (this.media.number + 1);
+			this.media.last  = apijs.config.slideshow.ids + '.' + this.media.album + '.' + this.totals[this.media.album];
+
+			// bouton précédent
+			if (document.getElementById(this.media.prev))
+				document.getElementById('prev').removeAttribute('disabled');
+			else
+				this.media.prev = null;
+
+			// bouton suivant
+			if (document.getElementById(this.media.next))
+				document.getElementById('next').removeAttribute('disabled');
+			else
+				this.media.next = null;
+		}
+	};
+
+
+
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// DÉFINITION DES ACTIONS DES BOUTONS ET DES TOUCHES DU CLAVIER (4)
+
+	// #### Action de la touche Début ###################################### event ## public ### //
+	// = révision : 4
+	// » Affiche la première photo ou vidéo
+	// » En provenance de la touche début
+	this.actionFirst = function () {
+
+		if ((this.media !== null) && (this.media.number > 0) && (this.media.number <= this.totals[this.media.album]))
+			this.showMedia(this.media.first);
+	};
+
+
+	// #### Action du bouton Précédent ##################################### event ## public ### //
+	// = révision : 14
+	// » Affiche la photo ou vidéo précédente
+	// » En provenance du dialogue photo/vidéo ou de la touche gauche
+	this.actionPrev = function () {
+
+		if ((this.media !== null) && (this.media.prev !== null) && (this.media.number > 0))
+			this.showMedia(this.media.prev);
+	};
+
+
+	// #### Action du bouton Suivant ####################################### event ## public ### //
+	// = révision : 13
+	// » Affiche la photo ou vidéo suivante
+	// » En provenance du dialogue photo/vidéo ou de la touche droite
+	this.actionNext = function () {
+
+		if ((this.media !== null) && (this.media.next !== null) && (this.media.number < this.totals[this.media.album]))
+			this.showMedia(this.media.next);
+	};
+
+
+	// #### Action de la touche Fin ######################################## event ## public ### //
+	// = révision : 4
+	// » Affiche la dernière photo ou vidéo
+	// » En provenance de la touche fin
+	this.actionLast = function () {
+
+		if ((this.media !== null) && (this.media.number >= 0) && (this.media.number < this.totals[this.media.album]))
+			this.showMedia(this.media.last);
+	};
+}
